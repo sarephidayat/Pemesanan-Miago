61249e7d1dbf15cdfbcf303c46b6dbb8de844d8a
MUHAMMAD SYARIFUDIN HIDAYAT <hidayatsyarifudin903@gmail.com>
2025-09-07 10:54:22 +0700

    kedua

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/home', function () {
+    return view('home');
+})->middleware(['auth', 'verified'])->name('home');
+
+require __DIR__ . '/auth.php';
